16534d1e03770729a7983ea55a46cf2faa4809e3
Antti <cursedtomb@LAPTOP-L7NICNE2.localdomain>
2020-03-02 22:19:45 +0200

    hevonen

--- src/shakki/nappulat/Ratsu.java
+++ src/shakki/nappulat/Ratsu.java
@@ -9,10 +9,97 @@ public class Ratsu extends Nappula
 		super(vari, 2, sijaintiX, sijaintiY);
 	}
 
-	@Override
-	protected void liiku()
+	protected void mahdollisetLiikkeet()
 	{
-		// TODO Auto-generated method stub
-		
+		if(x+2<=7 && y+1 <=7)
+		{
+			if(Peli.lauta[x+2][y+1] instanceof Nappula)
+			{
+				if(this.annaVari()!=Peli.lauta[x+2][y+1].annaVari())
+				{
+					liikkeet.add(new Ruutu(x+2,y+1));
+				}
+			}
+			else liikkeet.add(new Ruutu(x+2,y+1));
+		}
+		if(x+2<=7 && y-1 >=0)
+		{
+			if(Peli.lauta[x+2][y-1] instanceof Nappula)
+			{
+				if(this.annaVari()!=Peli.lauta[x+2][y-1].annaVari())
+				{
+					liikkeet.add(new Ruutu(x+2,y-1));
+				}
+			}
+			else liikkeet.add(new Ruutu(x+2,y-1));
+		}
+		if(x-2>=0 && y+1 <=7)
+		{
+			if(Peli.lauta[x-2][y+1] instanceof Nappula)
+			{
+				if(this.annaVari()!=Peli.lauta[x-2][y+1].annaVari())
+				{
+					liikkeet.add(new Ruutu(x-2,y+1));
+				}
+			}
+			else liikkeet.add(new Ruutu(x-2,y+1));
+		}
+		if(x-2>=0 && y-1 >=0)
+		{
+			if(Peli.lauta[x-2][y-1] instanceof Nappula)
+			{
+				if(this.annaVari()!=Peli.lauta[x-2][y-1].annaVari())
+				{
+					liikkeet.add(new Ruutu(x-2,y-1));
+				}
+			}
+			else liikkeet.add(new Ruutu(x-2,y-1));
+		}
+
+		if(x+1<=7 && y+2 <=7)
+		{
+			if(Peli.lauta[x+1][y+2] instanceof Nappula)
+			{
+				if(this.annaVari()!=Peli.lauta[x+1][y+2].annaVari())
+				{
+					liikkeet.add(new Ruutu(x+1,y+2));
+				}
+			}
+			else liikkeet.add(new Ruutu(x+1,y+2));
+		}
+		if(x+1<=7 && y-2 >=0)
+		{
+			if(Peli.lauta[x+1][y-2] instanceof Nappula)
+			{
+				if(this.annaVari()!=Peli.lauta[x+1][y-2].annaVari())
+				{
+					liikkeet.add(new Ruutu(x+1,y-2));
+				}
+			}
+			else liikkeet.add(new Ruutu(x+1,y-2));
+		}
+
+		if(x-1>=0 && y+2 <=7)
+		{
+			if(Peli.lauta[x-1][y+2] instanceof Nappula)
+			{
+				if(this.annaVari()!=Peli.lauta[x-1][y+2].annaVari())
+				{
+					liikkeet.add(new Ruutu(x-1,y+2));
+				}
+			}
+			else liikkeet.add(new Ruutu(x-1,y+2));
+		}
+		if(x-1>=0 && y-2 >=0)
+		{
+			if(Peli.lauta[x-1][y-2] instanceof Nappula)
+			{
+				if(this.annaVari()!=Peli.lauta[x-1][y-2].annaVari())
+				{
+					liikkeet.add(new Ruutu(x-1,y-2));
+				}
+			}
+			else liikkeet.add(new Ruutu(x-1,y-2));
+		}
 	}
 }
