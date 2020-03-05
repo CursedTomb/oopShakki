28fbdb15dbdf044134fd009db8dd2ca2815c4c54
Joona Kärkkäinen <joona.a.karkkainen@utu.fi>
2020-03-05 22:32:37 +0200

    Lähetti

--- src/shakki/nappulat/Lahetti.java
+++ src/shakki/nappulat/Lahetti.java
@@ -10,6 +10,36 @@ public class Lahetti extends Nappula
 	}
 	protected void mahdollisetLiikkeet()
 	{
-		return;
+		for(int i=-1; i==1; i=1)
+		{
+			// Väliaikainen x ja y joiden arvot muutettavissa, molemmat nousee yksi askel kerrallaan eikä kumpikaan voi nousta yli 7 tai laskea alle 0
+			for(int tempx = x, tempy = y; (tempx <= 7 && tempx >= 0) && (tempy <= 7 && tempy >= 0); tempx++, tempy+=i)
+			{
+				if(Peli.lauta[tempx][tempy] == null)
+					liikkeet.add(new Ruutu(tempx, y));
+				
+				else if(Peli.lauta[tempx][tempy] instanceof Nappula)
+                {
+                    if(Peli.lauta[tempx][tempy].annaVari() != this.vari)
+                        liikkeet.add(new Ruutu(tempx, y));
+                    
+                    break;
+                }
+			}
+			
+			for(int tempx = x, tempy = y; (tempx <= 7 && tempx >= 0) && (tempy <= 7 && tempy >= 0); tempx--, tempy+=i)
+			{
+				if(Peli.lauta[tempx][tempy] == null)
+					liikkeet.add(new Ruutu(tempx, y));
+				
+				else if(Peli.lauta[tempx][tempy] instanceof Nappula)
+                {
+                    if(Peli.lauta[tempx][tempy].annaVari() != this.vari)
+                        liikkeet.add(new Ruutu(tempx, y));
+                    
+                    break;
+                }
+			}
+		}
 	}
 }
