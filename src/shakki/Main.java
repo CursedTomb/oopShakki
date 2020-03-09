6b183f1282d65c7bd09d3f6d11fa0ec799c432ce
Antti <cursedtomb@LAPTOP-L7NICNE2.localdomain>
2020-03-09 08:32:53 +0200

    main kommentoitu

--- src/shakki/Main.java
+++ src/shakki/Main.java
@@ -6,14 +6,15 @@ public class Main
 {
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		// Kysyy onko uusi peli vai jatkaako ja antaa vastauksen peli objektille
 		Scanner skanneri = new Scanner(System.in);
-		System.out.print("Uusi peli vai jatka u/j: ");
+		System.out.print("Uusi peli vai jatka u/j: "); 
 		String s = skanneri.nextLine();
 		Peli p = new Peli(s);
 		p.printtaaLauta();
-		while(true)
+		while(true) // Käy vuoroja kunnes tulee true (luovututtaminen)
 			if(p.vuoro(p.annaVuoro())) break;
-		if(p.annaVuoro())
+		if(p.annaVuoro()) // Juhlistaa voittajan
 		{
 			System.out.println("Musta voitit pelin!");
 		}
